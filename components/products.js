0a37fd546fb7387bb4cc0c04dbe13da31d8da4d1
KinetekEnergy <transyt8@gmail.com>
2023-10-24 18:18:42 -0700

    spacing fixed

---
+++ components/products.js
@@ -0,0 +1,27 @@
+const cookiesList = [
+    {
+        title: "",
+        url: "",
+        description: "",
+        price: "",
+    },
+    {
+        title: "",
+        url: "",
+        description: "",
+        price: "",
+    },
+    {
+        title: "",
+        url: "",
+        description: "",
+        price: "",
+    },
+    {
+        title: "",
+        url: "",
+        description: "",
+        price: "",
+    },
+]
+
